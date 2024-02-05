5e09f741c5225b2becd81565aa910163fec6324d
Paul Pfeister <paul@pfeister.dev>
2024-02-04 21:52:21 -0500

    Adjust Wallpapers

--- assets/README.md
+++
@@ -1,5 +0,0 @@
-# Windex Assets
-
-### Attribution
-
-[Wallpaper 00](./Wallpaper_00.jpg) // Mohammad Alizade [\[source\]](https://unsplash.com/photos/a-blurry-image-of-a-blue-and-pink-background-XgeZu2jBaVI) [\[license\]](https://unsplash.com/license)
--- tweaks/autorun/Replace Wallpapers.ps1
+++ tweaks/autorun/Replace Wallpapers.ps1
@@ -3,22 +3,19 @@
 Replace the wallpaper for all user profiles on the system.
 .DESCRIPTION
 Replaces the current wallpaper for all existing and future users without tampering with the TrustedInstaller-protected defaults in %SystemRoot%\Web\Wallpaper.
-.PARAMETER WindexRoot
-The absolute or relative path to the Windex directory. Defaults to "..\..\".
-.PARAMETER SourceUri
-The absolute or relative path to the wallpaper image file you want to make the default.
-.PARAMETER StoredDir
-The absolute or relative path to the directory where you want the wallpaper to be stored. This must be a directory that all users have read access to. Defaults to "%SystemRoot%\Windex\Wallpapers\".
 .PARAMETER Undo
 Revert changes made by this script. Any users with their wallpaper set to $StoredUri will have their wallpaper set to the default Windows wallpaper.
 #>
 #Requires -RunAsAdministrator
 
 param (
-    [Parameter(Position = 0, Mandatory = $false)] [string] $SourceUri = "$(Split-Path $MyInvocation.MyCommand.Path -Parent)\..\assets\Wallpaper_00.jpg",
-    [Parameter(Position = 1, Mandatory = $false)] [string] $StoredUri = "$env:SystemRoot\Windex\Wallpapers\Wallpaper_00.jpg",
-    [Parameter(Position = 2, Mandatory = $false)] [switch] $Undo = $false
+    [Parameter(Position = 0, Mandatory = $false)] [switch] $Undo = $false
 )
+
+$SourceUri = "$(Split-Path $MyInvocation.MyCommand.Path -Parent)\..\..\assets\Wallpapers\*"
+$StoredUri = "$env:SystemRoot\Windex\Wallpapers\"
+$WallpaperToUse = "Wallpaper_01.jpg"
+
 
 New-Item -ItemType Directory -Path (Split-Path -Path $StoredUri -Parent) -Force -ErrorAction Stop | Out-Null
 Copy-Item -Path $SourceUri -Destination $StoredUri -Recurse -ErrorAction Stop | Out-Null
@@ -32,22 +29,22 @@ foreach ($SID in $KnownSIDs) {
         Set-ItemProperty -Path "registry::$SID\Control Panel\Desktop\" -Name Wallpaper -Value "$env:SystemRoot\Web\Wallpaper\Windows\img0.jpg"
         continue
     }
-    Set-ItemProperty -Path "registry::$SID\Control Panel\Desktop\" -Name Wallpaper -Value $StoredUri
+    Set-ItemProperty -Path "registry::$SID\Control Panel\Desktop\" -Name Wallpaper -Value "$StoredUri\$WallpaperToUse"
 }
 
 # This value is typically replaced by the default theme, but we'll set it here just in case.
 REG LOAD HKU\UserSkel "$env:SystemDrive\Users\Default\NTUSER.DAT"
-Set-ItemProperty -Path "registry::HKU\UserSkel\Control Panel\Desktop\" -Name Wallpaper -Value $StoredUri
+Set-ItemProperty -Path "registry::HKU\UserSkel\Control Panel\Desktop\" -Name Wallpaper -Value "$StoredUri\$WallpaperToUse"
 REG UNLOAD HKU\UserSkel
 
 # Changing the wallpaper used by the default aero theme
 takeown /f $env:SystemRoot\Resources\Themes\aero.theme
 icacls --% %SystemRoot%\Resources\Themes\aero.theme /grant "Administrators":m
 if ($Undo){
-    (Get-Content $env:SystemRoot\Resources\Themes\aero.theme).Replace('\Windex\Wallpapers\Wallpaper_00.jpg', '\web\wallpaper\Windows\img0.jpg') `
+    (Get-Content $env:SystemRoot\Resources\Themes\aero.theme).Replace("\Windex\Wallpapers\$WallpaperToUse", '\web\wallpaper\Windows\img0.jpg') `
     | Set-Content $env:SystemRoot\Resources\Themes\aero.theme
 } else {
-    (Get-Content $env:SystemRoot\Resources\Themes\aero.theme).Replace('\web\wallpaper\Windows\img0.jpg', '\Windex\Wallpapers\Wallpaper_00.jpg') `
+    (Get-Content $env:SystemRoot\Resources\Themes\aero.theme).Replace('\web\wallpaper\Windows\img0.jpg', "\Windex\Wallpapers\$WallpaperToUse") `
     | Set-Content $env:SystemRoot\Resources\Themes\aero.theme
 }
 icacls --% %SystemRoot%\Resources\Themes\aero.theme /setowner "NT SERVICE\TrustedInstaller"
